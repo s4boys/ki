f9ca87a42f2dfed32fa0f634b3a3eef5d749bb27
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-10-30 13:36:09 +0100

    reworked parser

--- WordCounter.py
+++ WordCounter.py
@@ -1,5 +1,6 @@
 #import EmailParser.EmailParser as EmailParser
 import EmailParser
+import NewEmailParser as nep
 
 
 def read_email(path):
@@ -9,10 +10,17 @@ def read_email(path):
 def main():
     output_path = "spam.arff"
     enron_mail = read_email("short_enron4.csv")
-    parser = EmailParser.EmailParser()
-    parser.parse_emails(enron_mail)
-    #parser.filter_attributes_by_occurrence(5)
-    parser.write_arff_file(output_path)
+
+
+    # parser = EmailParser.EmailParser()
+    # parser.parse_emails(enron_mail)
+    # #parser.filter_attributes_by_occurrence(5)
+    # parser.write_arff_file(output_path)
+
+    mails, wordcount = nep.parse_emails(enron_mail)
+    nep.write_arff_file(mails, wordcount, output_path)
+
+    
 
 
 if __name__ == "__main__":
