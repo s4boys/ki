c3530613e3b3d0065952ca8f07266fb4c6b85a5e
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-11-02 18:06:12 +0100

    use naive bayes on complex attributes and melt your ram

--- scikit.py
+++ scikit.py
@@ -9,21 +9,23 @@ def main():
     with open("short_enron4.csv") as enron_mail:
         mails, wordcount = nep.parse_emails(enron_mail)
 
-    # TODO: generate attributes that are currently
-    # only being generated when writing the arff file
+
+    # generate attributes
+    labels = list(map(lambda x: x.ham, mails))
+    # features = list(map(lambda x: (x.email_length, 1), mails))
+    features = list(map(lambda mail: Email.AttributeSet(mail, wordcount).as_list, mails))
+    
+    # calculate split
+    split = 0.99
+    split_index = int(len(features) * split)
 
     # train
-    labels = list(map(lambda x: x.ham, mails))
-    # TODO: pass more attributes (need to be generated first)
-    features = list(map(lambda x: (x.email_length, 1), mails))
     model = GaussianNB()
-    model.fit(features, labels)
+    model.fit(features[:split_index], labels)
 
-    # predict
-    predict_sample = [3]
-    predicted = model.predict([predict_sample])
+    # test / predict
+    predicted = model.predict(features[split_index:][0])
     print("Predicted: ", predicted, " for ", predict_sample)
-
 
 if __name__ == "__main__":
     main()
